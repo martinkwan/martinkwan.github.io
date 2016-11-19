7e0da4515502cce2fd8fb98cb6da068b85d39eef
Martin <martinkkwan@gmail.com>
2016-11-19 03:15:27 -0800

    (feat) Implement transparent nav on load

--- index.js
+++ index.js
@@ -2,9 +2,26 @@
 (function($) {
     "use strict"; // Start of use strict
 
+    $(document).ready(function(){
+       var scroll_start = 0;
+       var startchange = $('#page-top');
+       var offset = startchange.offset();
+        if (startchange.length){
+       $(document).scroll(function() {
+          scroll_start = $(this).scrollTop();
+          if(scroll_start > offset.top) {
+              $(".navbar-default").css('background-color', '#01579B');
+           } else {
+              $('.navbar-default').css('background-color', 'transparent');
+           }
+       });
+        }
+    });
+
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
+        $anchor.blur();
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
         }, 500, 'easeInOutExpo');
